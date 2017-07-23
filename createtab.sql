047a029be5a8de7e156b69b29f71583756c9d5ce
sairamkarthik <sairamkarthik_babji@srmuniv.edu.in>
2017-07-23 11:26:10 +0530

    Add files via upload

---
+++ createtab.sql
@@ -0,0 +1,13 @@
+CREATE TABLE RANDOM
+(
+    last_name   VARCHAR(25) NOT NULL,
+    first_name  VARCHAR(15) NOT NULL,
+    suffix      VARCHAR(5) NULL,
+    sex         VARCHAR(1) NULL,
+    city        VARCHAR(20) NOT NULL,
+    state       VARCHAR(2) NOT NULL,
+    country VARCHAR(15) NOT NULL,
+    age     int,
+    food VARCHAR(25) null,
+    hobby Varchar(10)
+)
