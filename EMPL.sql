974b22cbe40edf1750e935cee0f1aff9157f07b8
sairamkarthik <sairamkarthik_babji@srmuniv.edu.in>
2017-07-23 12:35:09 +0530

    Add files via upload

---
+++ EMPL.sql
@@ -0,0 +1,12 @@
+CREATE TABLE EMPLOYEE 
+( 
+last_name VARCHAR(25) NOT NULL,
+ first_name VARCHAR(15) NOT NULL,
+ suffix VARCHAR(5) NULL,
+ sex VARCHAR(1) NULL,
+ city VARCHAR(20) NOT NULL,
+ state VARCHAR(2) NOT NULL,
+ country VARCHAR(15) NOT NULL,
+ age int, food VARCHAR(25) null,
+ ID int(5) NOT NULL PRIMARY KEY
+ );
